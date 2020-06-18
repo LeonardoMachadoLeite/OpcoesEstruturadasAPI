36423ec06ae1d7fb3928cbcace90e8cc5bd0da85
AnaCarlaG <acarlagomes250@gmail.com>
2020-06-18 11:47:08 -0300

    correção do css na carteira e mudança de posicao tabela e gráfico

--- FrontEnd/src/app/carteira/carteira-item/carteira-item.component.ts
+++ FrontEnd/src/app/carteira/carteira-item/carteira-item.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit, Input, NgModule } from '@angular/core';
 @Component({
   selector: 'app-carteira-item',
   templateUrl: './carteira-item.component.html',
-  styleUrls: ['./carteira-item.component.scss']
+  styleUrls: ['./carteira-item.component.scss'],
+  preserveWhitespaces: true
 })
 export class CarteiraItemComponent implements OnInit {
 
